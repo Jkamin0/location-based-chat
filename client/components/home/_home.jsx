512f3c2892e0336e784872806603618c7c9f9cff
Jacob <jkamino@live.com>
2022-03-31 13:29:23 -0600

    post office hours, mvp is working

--- client/components/home/_home.jsx
+++ client/components/home/_home.jsx
@@ -19,18 +19,35 @@ export const Home = () => {
   const updatesRef = useRef([]);
 
   const [availableRooms, setAvailableRooms] = useState([]);
+  console.log('Rooms', chatRooms);
+  const distance = (lat1, lon1, lat2, lon2) => {
+    var p = 0.017453292519943295;
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p) / 2 + (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a));
+  };
+
+  const checkRooms = (location) => {
+    console.log(chatRooms);
+    const nearByRooms = chatRooms.filter((room) => {
+      const dist = distance(room.latitude, room.longitude, location.coords.latitude, location.coords.longitude);
+
+      console.log(dist);
+
+      return dist < 1000;
+    });
+    setAvailableRooms(nearByRooms);
+  };
 
   useEffect(async () => {
     const res = await api.get('/users/me');
-    const { chatRooms } = await api.get('/chat_rooms');
-    console.log(chatRooms);
-    setChatRooms(chatRooms);
+    const { chatRooms: rooms } = await api.get('/chat_rooms');
+    console.log('mychatrooms:');
+    console.log(rooms);
+    setChatRooms(rooms);
     setUser(res.user);
     setLoading(false);
-
-    setAvailableRooms(availableRooms);
-    checkRooms();
-    console.log(availableRooms);
   }, []);
 
   useEffect(() => {
@@ -38,6 +55,7 @@ export const Home = () => {
       (location) => {
         updatesRef.current.push(location);
         setUpdates([...updatesRef.current]);
+        checkRooms(location);
       },
       (err) => {
         setErrorMessage(err.message);
@@ -46,7 +64,7 @@ export const Home = () => {
     return () => {
       navigator.geolocation.clearWatch(watch);
     };
-  }, []);
+  }, [chatRooms]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -63,30 +81,6 @@ export const Home = () => {
     setName('');
   };
 
-  const distance = (lat1, lon1, lat2, lon2) => {
-    var p = 0.017453292519943295; // Math.PI / 180
-    var c = Math.cos;
-    var a = 0.5 - c((lat2 - lat1) * p) / 2 + (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
-
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-  };
-
-  const checkRooms = () => {
-    for (let i = 0; i < chatRooms.length; i++) {
-      let room = chatRooms[i];
-      let roomLat = room.latitude;
-      let roomLong = room.longitude;
-      let currentLat = updates[updates.length - 1].coords.latitude;
-      let currentLong = updates[updates.length - 1].coords.longitude;
-      let howFar = distance(roomLat, roomLong, currentLat, currentLong);
-      console.log(howFar);
-
-      if (distance < 1000) {
-        setAvailableRooms(...availableRooms, room);
-      }
-    }
-  };
-
   return (
     <div className="p-4">
       <h1>Welcome {user.firstName}</h1>
@@ -94,12 +88,13 @@ export const Home = () => {
         {errorMessage} {errorMessage && 'You need to enable GPS for this app to work'}
       </div>
       <div>
-        {updates[updates.length - 1].coords.latitude} {updates[updates.length - 1].coords.longitude}
+        {updates.length > 0 && updates[updates.length - 1].coords.latitude}{' '}
+        {updates.length && updates[updates.length - 1].coords.longitude}
       </div>
       <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       <Button onClick={createRoom}>Create Room</Button>
       <div>
-        {chatRooms.map((chatRoom) => (
+        {availableRooms.map((chatRoom) => (
           <div key={chatRoom.id}>
             <Link to={`/chat_rooms/${chatRoom.id}`}>{chatRoom.name}</Link>
           </div>
